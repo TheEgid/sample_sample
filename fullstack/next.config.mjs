99522f034c06d11ee8a15e5e369b98dc077674ba
TheEgid <egid.f1@gmail.com>
2024-07-21 16:39:42 +0300

    Fix

--- fullstack/next.config.mjs
+++ fullstack/next.config.mjs
@@ -3,4 +3,7 @@ export default {
     distDir: "build",
     reactStrictMode: true,
     onDemandEntries: { maxInactiveAge: 25 * 10000 },
+    images: {
+        domains: ['media.istockphoto.com'],
+    },
 };
--- fullstack/src/pages/index.tsx
+++ fullstack/src/pages/index.tsx
@@ -2,26 +2,50 @@ import React from "react";
 import { useUnit } from "effector-react";
 import Head from "next/head";
 import "react-toastify/dist/ReactToastify.css";
+import Image from "next/image"
 import { Button, Container } from "react-bootstrap";
 import { ToastContainer } from "react-toastify";
 import { EmptyLine } from "@/EmptyLine";
 import { $addBlogItemStatus, addBlogItemFx } from "@/model/some/state";
 
 const FixedWindow = () => {
+    const functionToExecute = (event: React.FormEvent) => {
+        event.preventDefault();
+        void addBlogItemFx();
+    };
+
     return (
-        <div className="fixed-window">
-            <p>Это прикрепленное окно</p>
+        <>
+
+            <div className="fixed-calc-window">
+                <p>
+                    <Button variant="secondary" onClick={functionToExecute}>
+                        Нажми меня
+                    </Button>
+                </p>
+                <p>Это прикрепленное окно 1</p>
+                <p>Это прикрепленное окно 2</p>
+            </div>
+        </>
+    );
+};
+
+const ImageComponent = () => {
+    return (
+        <div className="image-container">
+            <Image
+                src="https://media.istockphoto.com/id/185304274/fr/photo/chaton.jpg?s=2048x2048&w=is&k=20&c=BPWp3q_jnE_6YKdnN3MyRVAz01FnEj82PTYqwfZ_Jm4="
+                alt="Описание изображения"
+                className="image"
+                width={800}
+                height={500}
+            />
         </div>
     );
 };
 
 const Home: React.FC = () => {
     const { loading, error, data } = useUnit($addBlogItemStatus);
-
-    const functionToExecute = (event: React.FormEvent) => {
-        event.preventDefault();
-        void addBlogItemFx();
-    };
 
     return (
         <>
@@ -40,11 +64,10 @@ const Home: React.FC = () => {
                     <div className="chil">{error?.message || "без ошибок"}</div>
                     <EmptyLine />
                     <div className="chil">{(data.length > 0 && data) || "_"}</div>
-                    <EmptyLine />
-                    <Button variant="secondary" onClick={functionToExecute}>
-                        Нажми меня
-                    </Button>
                 </div>
+                <ImageComponent />
+                <ImageComponent />
+                <ImageComponent />
             </Container>
             <FixedWindow />
         </>
