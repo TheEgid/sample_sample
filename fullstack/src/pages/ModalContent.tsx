3ae9e243ffbac9cdf9192cbfd03f8118eaa6e0a2
TheEgid <egid.f1@gmail.com>
2024-07-25 16:24:06 +0300

    Simplify

--- fullstack/src/pages/ModalContent.tsx
+++ fullstack/src/pages/ModalContent.tsx
@@ -1,50 +1,34 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect } from "react";
 import { Fancybox as FancyboxNative } from "@fancyapps/ui";
 import { Button } from "react-bootstrap";
-import { createPortal } from "react-dom";
-
-const ModalContent = () => {
-    return (
-        <div>
-            <h2>Контент модального окна</h2>
-            <p>Это пример содержимого, которое отображается в модальном окне.</p>
-            <Button onClick={() => alert("Действие!")}>Кнопка</Button>
-        </div>
-    );
-};
 
 const FancyboxExample = () => {
-    const modalContainer = useRef<HTMLDivElement | null>(null);
-    const [isClient, setIsClient] = useState(false);
-
     useEffect(() => {
-        setIsClient(true);
+        FancyboxNative.bind("[data-fancybox]", {});
 
-        if (typeof window !== "undefined") {
-            const container = document.createElement("div");
-
-            document.body.appendChild(container);
-            modalContainer.current = container;
-
-            return () => {
-                FancyboxNative.destroy();
-                if (modalContainer.current) {
-                    document.body.removeChild(modalContainer.current);
-                }
-            };
-        }
+        return () => {
+            FancyboxNative.destroy();
+        };
     }, []);
-
-    const openModal = () => {
-        if (modalContainer.current) { FancyboxNative.show([{ src: modalContainer.current, type: "html" }]); }
-    };
 
     return (
         <div>
             <h2>Example of a Modal with React Component</h2>
-            <Button onClick={openModal}>Open Modal</Button>
-            {/* Render the modal content only if in the client-side environment */}
-            {isClient && modalContainer.current && createPortal(<ModalContent />, modalContainer.current)}
+            <Button
+                data-fancybox
+                data-src="#hidden-content"
+                data-type="inline"
+            >
+                Open Modal
+            </Button>
+
+            <div style={{ display: "none" }}>
+                <div id="hidden-content">
+                    <h2>Контент модального окна</h2>
+                    <p>Это пример содержимого, которое отображается в модальном окне.</p>
+                    {/* <Button onClick={() => alert("Действие!")}>Кнопка</Button> */}
+                </div>
+            </div>
         </div>
     );
 };
